76042ce24c1b4f671175f887f58b73f13e9c78e9
Camille Berrido <berridocamille@gmail.com>
2024-06-26 17:20:17 +0100

    adding description + visualizations

--- linear_reg_crabs/linear_regression_crabs.py
+++ linear_reg_crabs/linear_regression_crabs.py
@@ -1,11 +1,19 @@
 #-----------------------------------------------------------------------------#
 # Linear regression - based on kaggle competition
 # https://www.kaggle.com/competitions/playground-series-s3e16/overview
+#
+#
+# The goal of this competition is to use regression to predict the age of
+# crabs given physical attributes.
+# First, we will try to understand the data and visualise the relationships
+# between the different variables
+# After that, we will try to create a model to predict the age of
+# the test dataset
 #-----------------------------------------------------------------------------#
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
+import seaborn as sns
 
 #-----------------------------------------------------------------------------#
 # Importing data
@@ -16,6 +24,56 @@ outpath = str(path+"outputs/")
 
 train = pd.read_csv(str(inpath+"train.csv"))
 test = pd.read_csv(str(inpath+"test.csv"))
+
+#-----------------------------------------------------------------------------#
+# Data exploration and visualizations
+#-----------------------------------------------------------------------------#
+
+# Distribution of Age
+plt.figure(figsize=(10, 6))
+plt.hist(train['Age'], bins=15, color='skyblue', edgecolor='black')
+plt.title('Distribution of Age')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
+
+# Here, we see that the number of crabs is centered around 10 years of age
+
+# Sex Distribution
+plt.figure(figsize=(10, 6))
+train['Sex'].value_counts().plot(kind='bar', color=['lightcoral', 'lightgreen', 'lightblue'])
+plt.title('Distribution of Abalones by Sex')
+plt.xlabel('Sex')
+plt.ylabel('Count')
+plt.grid(True)
+plt.show()
+
+# We notice that there are slightly more males than females and infants in our dataset
+
+# Length vs. Weight
+plt.figure(figsize=(10, 6))
+plt.scatter(train['Length'], train['Weight'], alpha=0.5, color='purple')
+plt.title('Length vs. Weight')
+plt.xlabel('Length')
+plt.ylabel('Weight')
+plt.grid(True)
+plt.show()
+
+# Here, we notice that there is a positive relationship between the length and the weight of the crabs,
+# where animals with greater lengths tend to also be heavier
+
+# Correlation Heatmap
+plt.figure(figsize=(12, 8))
+correlation_matrix = train.corr()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title('Correlation Heatmap')
+plt.show()
+
+# This correlation matrix helps identify which numerical variables are related and to what extent.
+# Strong correlations are highlighted between variables like Length, Diameter, and Weight
+# We see that as one of these measurements increases, the others tend to increase as well.
+
 
 #-----------------------------------------------------------------------------#
 # Feature selection and preparing data
